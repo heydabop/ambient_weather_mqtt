1855dc0912cc9e8be0ef0a9040d3359f6dcc1140
heydabop <heydabop@gmail.com>
2023-04-28 11:12:28 -0500

    use structs for config payloads, add unique_ids and device to config payloads

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use axum::{
     Router,
 };
 use mqtt::client::Client as MqttClient;
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 use std::{
     collections::HashMap,
     net::SocketAddr,
@@ -16,8 +16,20 @@ use tracing::{error, info};
 
 type AppState = Arc<Mutex<MqttClient>>;
 
+#[derive(Serialize)]
+struct SensorConfig<'a> {
+    pub name: &'static str,
+    pub unique_id: &'static str,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub device_class: Option<&'static str>,
+    pub device: &'a HashMap<&'static str, &'static str>,
+    pub state_topic: &'static str,
+    pub unit_of_measurement: Option<&'static str>,
+    pub state_class: &'static str,
+}
+
 #[derive(Deserialize)]
-struct Config {
+struct AppConfig {
     pub broker_address: String,
     pub client_id: String,
     pub username: String,
@@ -28,7 +40,7 @@ struct Config {
 async fn main() {
     tracing_subscriber::fmt::init();
 
-    let cfg: Config = match tokio::fs::read_to_string("config.toml").await {
+    let cfg: AppConfig = match tokio::fs::read_to_string("config.toml").await {
         Ok(s) => match toml::from_str(&s) {
             Ok(t) => t,
             Err(e) => {
@@ -48,24 +60,248 @@ async fn main() {
         exit(1);
     }
 
-    mqtt_client.publish("homeassistant/sensor/ambientWeather/temperature/config", r#"{"name":"Outside Temperature","device_class":"temperature","state_topic":"homeassistant/sensor/ambientWeather/temperature/state","unit_of_measurement":"°F","state_class":"measurement"}"#);
-    mqtt_client.publish("homeassistant/sensor/ambientWeather/humidity/config", r#"{"name":"Outside Humidity","device_class":"humidity","state_topic":"homeassistant/sensor/ambientWeather/humidity/state","unit_of_measurement":"%","state_class":"measurement"}"#);
-    mqtt_client.publish("homeassistant/sensor/ambientWeather/dewPoint/config", r#"{"name":"Outside Dew Point","device_class":"temperature","state_topic":"homeassistant/sensor/ambientWeather/dewPoint/state","unit_of_measurement":"°F","state_class":"measurement"}"#);
-    mqtt_client.publish("homeassistant/sensor/ambientWeather/windChill/config", r#"{"name":"Wind Chill","device_class":"temperature","state_topic":"homeassistant/sensor/ambientWeather/windChill/state","unit_of_measurement":"°F","state_class":"measurement"}"#);
-    mqtt_client.publish("homeassistant/sensor/ambientWeather/windDir/config", r#"{"name":"Wind Dir","state_topic":"homeassistant/sensor/ambientWeather/windDir/state","unit_of_measurement":"°"}"#);
-    mqtt_client.publish("homeassistant/sensor/ambientWeather/windSpeed/config", r#"{"name":"Wind Speed","device_class":"wind_speed","state_topic":"homeassistant/sensor/ambientWeather/windSpeed/state","unit_of_measurement":"mph","state_class":"measurement"}"#);
-    mqtt_client.publish("homeassistant/sensor/ambientWeather/windGust/config", r#"{"name":"Wind Gust","device_class":"wind_speed","state_topic":"homeassistant/sensor/ambientWeather/windGust/state","unit_of_measurement":"mph","state_class":"measurement"}"#);
-    mqtt_client.publish("homeassistant/sensor/ambientWeather/rainHourly/config", r#"{"name":"Hourly Rain Rate","device_class":"precipitation_intensity","state_topic":"homeassistant/sensor/ambientWeather/rainHourly/state","unit_of_measurement":"in/h","state_class":"measurement"}"#);
-    mqtt_client.publish("homeassistant/sensor/ambientWeather/rainDaily/config", r#"{"name":"Daily Rain","device_class":"precipitation","state_topic":"homeassistant/sensor/ambientWeather/rainDaily/state","unit_of_measurement":"in","state_class":"total_increasing"}"#);
-    mqtt_client.publish("homeassistant/sensor/ambientWeather/rainWeekly/config", r#"{"name":"Weekly Rain","device_class":"precipitation","state_topic":"homeassistant/sensor/ambientWeather/rainWeekly/state","unit_of_measurement":"in","state_class":"total_increasing"}"#);
-    mqtt_client.publish("homeassistant/sensor/ambientWeather/rainMonthly/config", r#"{"name":"Monthly Rain","device_class":"precipitation","state_topic":"homeassistant/sensor/ambientWeather/rainMonthly/state","unit_of_measurement":"in","state_class":"total_increasing"}"#);
-    mqtt_client.publish("homeassistant/sensor/ambientWeather/rainLifetime/config", r#"{"name":"Lifetime Rain","device_class":"precipitation","state_topic":"homeassistant/sensor/ambientWeather/rainLifetime/state","unit_of_measurement":"in","state_class":"total_increasing"}"#);
-    mqtt_client.publish("homeassistant/sensor/ambientWeather/solarRadiation/config", r#"{"name":"Solar Radiation","device_class":"irradiance","state_topic":"homeassistant/sensor/ambientWeather/solarRadiation/state","unit_of_measurement":"W/m²","state_class":"measurement"}"#);
-    mqtt_client.publish("homeassistant/sensor/ambientWeather/UV/config", r#"{"name":"UV Index","state_topic":"homeassistant/sensor/ambientWeather/UV/state","state_class":"measurement"}"#);
-    mqtt_client.publish("homeassistant/sensor/ambientWeather/kitchenTemperature/config", r#"{"name":"Kitchen Temperature","device_class":"temperature","state_topic":"homeassistant/sensor/ambientWeather/kitchenTemperature/state","unit_of_measurement":"°F","state_class":"measurement"}"#);
-    mqtt_client.publish("homeassistant/sensor/ambientWeather/kitchenHumidity/config", r#"{"name":"Kitchen Humidity","device_class":"humidity","state_topic":"homeassistant/sensor/ambientWeather/kitchenHumidity/state","unit_of_measurement":"%","state_class":"measurement"}"#);
-    mqtt_client.publish("homeassistant/sensor/ambientWeather/pressure/config", r#"{"name":"Outside Pressure","device_class":"pressure","state_topic":"homeassistant/sensor/ambientWeather/pressure/state","unit_of_measurement":"hPa","state_class":"measurement"}"#);
-    mqtt_client.publish("homeassistant/sensor/ambientWeather/relativePressure/config", r#"{"name":"Outside Relative Pressure","device_class":"pressure","state_topic":"homeassistant/sensor/ambientWeather/relativePressure/state","unit_of_measurement":"hPa","state_class":"measurement"}"#);
+    let device = HashMap::from([
+        ("identifiers", "ambw_mqtt"),
+        ("manufacturer", "Ambient Weather"),
+        ("model", "WS-2902"),
+        ("name", "MQTT Weather Station"),
+        ("via_device", "ambient_weather_mqtt"),
+    ]);
+
+    publish_sensor_config(
+        &mqtt_client,
+        "temperature",
+        &SensorConfig {
+            name: "Outside Temperature",
+            unique_id: "ambw_mqtt_outside_temp",
+            device_class: Some("temperature"),
+            state_topic: "homeassistant/sensor/ambientWeather/temperature/state",
+            unit_of_measurement: Some("°F"),
+            state_class: "measurement",
+            device: &device,
+        },
+    );
+    publish_sensor_config(
+        &mqtt_client,
+        "humidity",
+        &SensorConfig {
+            name: "Outside Humidity",
+            unique_id: "ambw_mqtt_outside_hum",
+            device_class: Some("humidity"),
+            state_topic: "homeassistant/sensor/ambientWeather/humidity/state",
+            unit_of_measurement: Some("%"),
+            state_class: "measurement",
+            device: &device,
+        },
+    );
+    publish_sensor_config(
+        &mqtt_client,
+        "dewPoint",
+        &SensorConfig {
+            name: "Outside Dew Point",
+            unique_id: "ambw_mqtt_outside_dew",
+            device_class: Some("temperature"),
+            state_topic: "homeassistant/sensor/ambientWeather/dewPoint/state",
+            unit_of_measurement: Some("°F"),
+            state_class: "measurement",
+            device: &device,
+        },
+    );
+    publish_sensor_config(
+        &mqtt_client,
+        "windChill",
+        &SensorConfig {
+            name: "Wind Chill",
+            unique_id: "ambw_mqtt_wind_chill",
+            device_class: Some("temperature"),
+            state_topic: "homeassistant/sensor/ambientWeather/windChill/state",
+            unit_of_measurement: Some("°F"),
+            state_class: "measurement",
+            device: &device,
+        },
+    );
+    publish_sensor_config(
+        &mqtt_client,
+        "windDir",
+        &SensorConfig {
+            name: "Wind Dir",
+            unique_id: "ambw_mqtt_wind_dir",
+            device_class: None,
+            state_topic: "homeassistant/sensor/ambientWeather/windDir/state",
+            unit_of_measurement: Some("°"),
+            state_class: "measurement",
+            device: &device,
+        },
+    );
+    publish_sensor_config(
+        &mqtt_client,
+        "windSpeed",
+        &SensorConfig {
+            name: "Wind Speed",
+            unique_id: "ambw_mqtt_wind_speed",
+            device_class: Some("wind_speed"),
+            state_topic: "homeassistant/sensor/ambientWeather/windSpeed/state",
+            unit_of_measurement: Some("mph"),
+            state_class: "measurement",
+            device: &device,
+        },
+    );
+    publish_sensor_config(
+        &mqtt_client,
+        "windGust",
+        &SensorConfig {
+            name: "Wind Gust",
+            unique_id: "ambw_mqtt_wind_gust",
+            device_class: Some("wind_speed"),
+            state_topic: "homeassistant/sensor/ambientWeather/windGust/state",
+            unit_of_measurement: Some("mph"),
+            state_class: "measurement",
+            device: &device,
+        },
+    );
+    publish_sensor_config(
+        &mqtt_client,
+        "rainHourly",
+        &SensorConfig {
+            name: "Hourly Rain Rate",
+            unique_id: "ambw_mqtt_hourly_rain",
+            device_class: Some("precipitation_intensity"),
+            state_topic: "homeassistant/sensor/ambientWeather/rainHourly/state",
+            unit_of_measurement: Some("in/h"),
+            state_class: "measurement",
+            device: &device,
+        },
+    );
+    publish_sensor_config(
+        &mqtt_client,
+        "rainDaily",
+        &SensorConfig {
+            name: "Daily Rain",
+            unique_id: "ambw_mqtt_daily_rain",
+            device_class: Some("precipitation"),
+            state_topic: "homeassistant/sensor/ambientWeather/rainDaily/state",
+            unit_of_measurement: Some("in"),
+            state_class: "total_increasing",
+            device: &device,
+        },
+    );
+    publish_sensor_config(
+        &mqtt_client,
+        "rainWeekly",
+        &SensorConfig {
+            name: "Weekly Rain",
+            unique_id: "ambw_mqtt_weekly_rain",
+            device_class: Some("precipitation"),
+            state_topic: "homeassistant/sensor/ambientWeather/rainWeekly/state",
+            unit_of_measurement: Some("in"),
+            state_class: "total_increasing",
+            device: &device,
+        },
+    );
+    publish_sensor_config(
+        &mqtt_client,
+        "rainMonthly",
+        &SensorConfig {
+            name: "Monthly Rain",
+            unique_id: "ambw_mqtt_monthyl_rain",
+            device_class: Some("precipitation"),
+            state_topic: "homeassistant/sensor/ambientWeather/rainMonthly/state",
+            unit_of_measurement: Some("in"),
+            state_class: "total_increasing",
+            device: &device,
+        },
+    );
+    publish_sensor_config(
+        &mqtt_client,
+        "rainLifetime",
+        &SensorConfig {
+            name: "Lifetime Rain",
+            unique_id: "ambw_mqtt_lifetime_rain",
+            device_class: Some("precipitation"),
+            state_topic: "homeassistant/sensor/ambientWeather/rainLifetime/state",
+            unit_of_measurement: Some("in"),
+            state_class: "total_increasing",
+            device: &device,
+        },
+    );
+    publish_sensor_config(
+        &mqtt_client,
+        "solarRadiation",
+        &SensorConfig {
+            name: "Solar Radiation",
+            unique_id: "ambw_mqtt_solar_rad",
+            device_class: Some("irradiance"),
+            state_topic: "homeassistant/sensor/ambientWeather/solarRadiation/state",
+            unit_of_measurement: Some("W/m²"),
+            state_class: "measurement",
+            device: &device,
+        },
+    );
+    publish_sensor_config(
+        &mqtt_client,
+        "UV",
+        &SensorConfig {
+            name: "UV Index",
+            unique_id: "ambw_mqtt_uv",
+            device_class: None,
+            state_topic: "homeassistant/sensor/ambientWeather/UV/state",
+            unit_of_measurement: None,
+            state_class: "measurement",
+            device: &device,
+        },
+    );
+    publish_sensor_config(
+        &mqtt_client,
+        "kitchenTemperature",
+        &SensorConfig {
+            name: "Kitchen Temperature",
+            unique_id: "ambw_mqtt_indoor_temp",
+            device_class: Some("temperature"),
+            state_topic: "homeassistant/sensor/ambientWeather/kitchenTemperature/state",
+            unit_of_measurement: Some("°F"),
+            state_class: "measurement",
+            device: &device,
+        },
+    );
+    publish_sensor_config(
+        &mqtt_client,
+        "kitchenHumidity",
+        &SensorConfig {
+            name: "Kitchen Humidity",
+            unique_id: "ambw_mqtt_indoor_hum",
+            device_class: Some("humidity"),
+            state_topic: "homeassistant/sensor/ambientWeather/kitchenHumidity/state",
+            unit_of_measurement: Some("%"),
+            state_class: "measurement",
+            device: &device,
+        },
+    );
+    publish_sensor_config(
+        &mqtt_client,
+        "pressure",
+        &SensorConfig {
+            name: "Outside Pressure",
+            unique_id: "ambw_mqtt_abs_press",
+            device_class: Some("atmospheric_pressure"),
+            state_topic: "homeassistant/sensor/ambientWeather/pressure/state",
+            unit_of_measurement: Some("hPa"),
+            state_class: "measurement",
+            device: &device,
+        },
+    );
+    publish_sensor_config(
+        &mqtt_client,
+        "relativePressure",
+        &SensorConfig {
+            name: "Outside Relative Pressure",
+            unique_id: "ambw_mqtt_rel_press",
+            device_class: Some("atmospheric_pressure"),
+            state_topic: "homeassistant/sensor/ambientWeather/relativePressure/state",
+            unit_of_measurement: Some("hPa"),
+            state_class: "measurement",
+            device: &device,
+        },
+    );
 
     let state = Arc::new(Mutex::new(mqtt_client));
 
@@ -115,6 +351,7 @@ pub async fn handle_weather_update(
     publish_f32(&client, &params, "totalrainin", "rainLifetime", 3);
     publish_f32(&client, &params, "solarradiation", "solarRadiation", 1);
     publish_i32(&client, &params, "UV", "UV");
+    publish_f32(&client, &params, "indoortempf", "kitchenTemperature", 1);
     publish_i32(&client, &params, "indoorhumidity", "kitchenHumidity");
 
     if let Some(val) = params.get("absbaromin") {
@@ -125,6 +362,7 @@ pub async fn handle_weather_update(
                 client.publish(
                     "homeassistant/sensor/ambientWeather/pressure/state",
                     &payload,
+                    false,
                 )
             }
             Err(e) => {
@@ -143,6 +381,7 @@ pub async fn handle_weather_update(
                 client.publish(
                     "homeassistant/sensor/ambientWeather/relativePressure/state",
                     &payload,
+                    false,
                 )
             }
             Err(e) => {
@@ -171,6 +410,7 @@ fn publish_f32(
                 client.publish(
                     &format!("homeassistant/sensor/ambientWeather/{topic}/state"),
                     &payload,
+                    false,
                 )
             }
             Err(e) => {
@@ -188,7 +428,11 @@ fn publish_i32(client: &MqttClient, params: &HashMap<String, String>, key: &str,
             Ok(parsed) => {
                 let payload = parsed.to_string();
                 info!(topic, payload, "publishing");
-                client.publish(topic, &payload);
+                client.publish(
+                    &format!("homeassistant/sensor/ambientWeather/{topic}/state"),
+                    &payload,
+                    false,
+                );
             }
             Err(e) => {
                 error!(%e, val, key, "unable to parse i32 from param");
@@ -197,4 +441,14 @@ fn publish_i32(client: &MqttClient, params: &HashMap<String, String>, key: &str,
     } else {
         error!(key, "missing value in params");
     }
+}
+
+fn publish_sensor_config(client: &MqttClient, topic: &str, config: &SensorConfig) {
+    let payload = serde_json::to_string(config).unwrap();
+    info!(topic, payload, "publishing config");
+    client.publish(
+        &format!("homeassistant/sensor/ambientWeather/{topic}/config"),
+        &payload,
+        true,
+    );
 }
